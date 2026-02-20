58ca04c5665dbc497c9502c6148d4b62f3e70f63
Ethan Campbell <ethan.e.campbell@gmail.com>
2026-02-19 22:32:09 -0500

    Encrypt uses file upload and data from file upload as public key entry

--- backend/encrypt.py
+++ backend/encrypt.py
@@ -13,11 +13,13 @@ def create_gpg():
 def encrypt_file(file_path, public_key_path):
     try:
         print ("Starting file encryption process....")
+        print("public Key ", public_key_path)
         gpg = create_gpg()
 
-        import_result = gpg.import_keys_file(public_key_path)
+        import_result = gpg.import_keys(public_key_path)
         print("Import Result: ", import_result.fingerprints)
-        if not import_result.fingerprints: raise ValueError("No valid public keys were imported")
+        if not import_result.fingerprints: 
+            raise ValueError("No valid public keys were imported")
         result = gpg.encrypt_file(open(file_path, 'rb'), recipients=import_result.fingerprints,  output=file_path + '.gpg')
     except Exception as e:
         print("An error occurred during file encryption: ", e)
--- frontend/encrypt_page.py
+++ frontend/encrypt_page.py
@@ -2,17 +2,17 @@ import streamlit as st
 from backend.encrypt import encrypt_file as ef
 enable_inputs = False
 file_path = ""
-pgp_public_key = ""
+pgp_public_key_file = ""
 
-st.write("welcome to the encrypt page.")
+st.title("Welcome to the encrypt page.")
 st.write("Please answer these questions to encrypt your file.")
 file_path = st.text_input("Enter the path to the file you want to encrypt:")
-pgp_public_key = st.text_input("please enter the full file path of the public key you want to encrypt with: ")
-if st.button("Submit", key="file_encrypt_submit"):
-    result = ef(file_path,pgp_public_key)
-    #st.write(e)
-    st.write(result)
+pgp_public_key_file = st.file_uploader("please upload the public key you want to encrypt with:") 
 
-#if st.button("Encrypt a file", key="file_encrypt_show") and enable_inputs == False:
-#    enable_inputs = True
-#    show_encrypt_inputs()
+if pgp_public_key_file: 
+    st.write("You selected:", pgp_public_key_file.name)
+    pgp_public_key_data = pgp_public_key_file.read()
+
+if st.button("Submit", key="file_encrypt_submit"):
+    result = ef(file_path,file_bytes)
+    st.write(result)
