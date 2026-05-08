fb04037f2cba6a873dbac9ef8a081bb996120a97
Ethan Campbell <ethan.e.campbell@gmail.com>
2026-05-07 20:27:43 -0400

    remove pgp from send

--- backend/send.py
+++ backend/send.py
@@ -4,17 +4,9 @@ Author: Ethan Campbell
 Date: 28-Mar-2026
 Description: Dends files using SFTP.
 """
-import gnupg
 import paramiko
 
-def create_gpg():
-    try:
-        print("Initializing GPG...")
-        gpg = gnupg.GPG()
-        return gpg
-    except Exception as e:
-        print("An error occurred while initializing GPG: ", e)
-        exit(1)
+
 
 
 def send_file(file_path):
