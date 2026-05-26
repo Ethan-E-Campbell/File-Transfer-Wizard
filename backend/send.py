465877ae0fd388f34c96d6b657f61ca42e9407de
Ethan Campbell <ethan.e.campbell@gmail.com>
2026-05-25 22:55:40 -0400

    Adding fully working file send protocol

--- backend/send.py
+++ backend/send.py
@@ -11,28 +11,32 @@ def sftp_client_setup(hostname, port, username, password):
     try:
         print("Setting up SFTP client...")
         transport = paramiko.client.SSHClient()
+        # This should be fixed later...to ask if the user trusts the key. 
+        # Or to use some kind of host key management system.
         transport.set_missing_host_key_policy(paramiko.client.AutoAddPolicy())
         transport.connect(hostname=hostname, port=port,
                           username=username, password=password)
         return transport.open_sftp()
     except Exception as e:
         print("An error occurred while connecting to the SFTP server: ", e)
-        return
+        return e
 
 
 def send_file(file_name_and_path, file_send_path, hostname, port):
-    print("send file will happen here")
-    sftp = sftp_client_setup(hostname, port, "ec",
-                             "ec")
-    if sftp:
-        try:
-            print("Sending file...")
-            print(f"File name: {file_name_and_path}")
-            print(f"File path: {file_send_path}")
-            # sftp.mkdir('/upload')
-            sftp.put(file_name_and_path, file_send_path)
-            print("File sent successfully!")
-        except Exception as e:
-            print("An error occurred while sending the file: ", e)
-        finally:
-            sftp.close()
+    try:
+        sftp = sftp_client_setup(hostname, port, "ec",
+                                 "ec")
+        if isinstance(sftp, Exception):
+            return sftp
+
+        # print("Sending file...")
+        # print(f"File name: {file_name_and_path}")
+        # print(f"File path: {file_send_path}")
+        # sftp.mkdir('/upload')
+        sftp.put(file_name_and_path, file_send_path)
+        print("File sent successfully!")
+        sftp.close()
+        return None
+    except Exception as e:
+        print("An error occurred while sending the file: ", e)
+        return e
